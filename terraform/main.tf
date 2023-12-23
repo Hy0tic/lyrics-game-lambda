2bf9d551a0ecd14f1451ad2d55478405445a3ae8
Hy0tic <vothanhdat278@gmail.com>
2023-12-23 03:48:21 -0500

    fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,33 @@ terraform {
 
 provider "aws" {
     region  = "us-east-1"
-    access_key = "AKIA5KP6QNH7ZR2PLOXA"
-    secret_key = "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"
 }
 
 
 resource "aws_lambda_function" "lyrics_game_lambda" {
-    function_name = "lyrics-game-lambda"
-    role = "arn:aws:iam::915898657279:role/service-role/lyrics-game-lambda-role-5enu4vjm"
-    runtime = "dotnet6"
-    handler = "lyrics-game-lambda::lyrics_game_lambda.Function::FunctionHandler"
-    memory_size      = 256
-    timeout          = 30
-    filename = "../src/lyrics-game-lambda/bin/Release/net6.0/lyrics-game-lambda.zip"
+    architectures                  = [
+        "x86_64",
+    ]
+    function_name                  = "lyrics-game-lambda"
+    handler                        = "lyrics-game-lambda::lyrics_game_lambda.Function::FunctionHandler"
+    layers                         = []
+    memory_size                    = 256
+    package_type                   = "Zip"
+    reserved_concurrent_executions = -1
+    role                           = "arn:aws:iam::915898657279:role/service-role/lyrics-game-lambda-role-5enu4vjm"
+    runtime                        = "dotnet6"
+    skip_destroy                   = false
+    source_code_hash               = "RsNuSKTTq+wqxzv10l4qvtn0Az2bubmkkjGh5+1/YH8="
+    tags                           = {}
+    tags_all                       = {}
+    timeout                        = 30
+    filename                       = "./publish/lyrics-game-lambda.zip"
+
+    ephemeral_storage {
+        size = 512
+    }
+
+    tracing_config {
+        mode = "PassThrough"
+    }
 }
